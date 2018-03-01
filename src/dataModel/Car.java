67312b2462c6f564d8edcceb67ec2b36adffbc79
Unknown <louis.lalleau@gmail.com>
2018-03-01 22:27:55 +0100

    test gildas
    test gil

--- src/dataModel/Car.java
+++ src/dataModel/Car.java
@@ -8,17 +8,12 @@ public class Car {
 	int id, nextAvailable;
 	public List<Ride> rides;
 	public Position pos;
-	public Position[] planning;
 
 	public Car(int id, int nbMaxSteps) {
 		this.id = id;
 		nextAvailable = 0;
 		rides = new LinkedList<>();
 		pos = new Position(0, 0);
-		planning = new Position[nbMaxSteps];
-		for (int i = 0; i < nbMaxSteps; i++) {
-			planning[i] = null;
-		}
 	}
 
 	public boolean isAvailable(int currentStep) {
@@ -40,22 +35,6 @@ public class Car {
 	public boolean rideIsPossible(int currentStep, Ride goal) {
 		int total = endTimeRide(currentStep, goal);
 		return total <= goal.latest;
-	}
-
-	// Check available from start to end
-	public boolean isFreeBetween(int start, int end) {
-		for (int i = start; i <= end; i++) {
-			if (planning[i] != null) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public void setUnavailable(int from, int to, Position position) {
-		for (int i = from; i <= to; i++) {
-			planning[i] = position;
-		}
 	}
 
 }
