87944d0e10f0ad82182675d67bf8e1cf0e17b925
Unknown <louis.lalleau@gmail.com>
2018-03-01 22:26:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/dataModel/Car.java
+++ src/dataModel/Car.java
@@ -8,36 +8,54 @@ public class Car {
 	int id, nextAvailable;
 	public List<Ride> rides;
 	public Position pos;
-	
-	
-	public Car(int id) {
+	public Position[] planning;
+
+	public Car(int id, int nbMaxSteps) {
 		this.id = id;
 		nextAvailable = 0;
 		rides = new LinkedList<>();
-
-    pos = new Position(0,0);
+		pos = new Position(0, 0);
+		planning = new Position[nbMaxSteps];
+		for (int i = 0; i < nbMaxSteps; i++) {
+			planning[i] = null;
+		}
 	}
-	
+
 	public boolean isAvailable(int currentStep) {
-		if(currentStep < this.nextAvailable) {
+		if (currentStep < this.nextAvailable) {
 			return false;
-		}else {
+		} else {
 			return true;
 		}
 	}
-	
+
 	public int distanceToRide(Ride goal) {
 		return goal.start.distance(pos);
 	}
-	
+
 	public int endTimeRide(int currentStep, Ride ride) {
 		return this.distanceToRide(ride) + ride.start.distance(ride.finish);
 	}
-	
+
 	public boolean rideIsPossible(int currentStep, Ride goal) {
 		int total = endTimeRide(currentStep, goal);
 		return total <= goal.latest;
 	}
-	
+
+	// Check available from start to end
+	public boolean isFreeBetween(int start, int end) {
+		for (int i = start; i <= end; i++) {
+			if (planning[i] != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void setUnavailable(int from, int to, Position position) {
+		for (int i = from; i <= to; i++) {
+			planning[i] = position;
+		}
+	}
 
 }
--- src/dataModel/Simulation.java
+++ src/dataModel/Simulation.java
@@ -8,7 +8,7 @@ import java.io.Writer;
 import java.util.List;
 
 public class Simulation {
-	public int currentStep=0;
+	public int currentStep = 0;
 	public int rows, columns, nbVehicules, nbRides, bonus, nbMaxSteps;
 	public List<Car> cars;
 	public List<Ride> rides;
@@ -40,52 +40,52 @@ public class Simulation {
 		}
 
 		out.close();
-	}	
-	
+	}
+
 	public void resolve() {
 		Car c;
 		Iterator<Ride> i = rides.iterator();
-		while(i.hasNext()) {
+		while (i.hasNext()) {
 			Ride r = i.next();
 			c = this.getBestCarForRide(r);
-			if(c != null) {
+			if (c != null) {
 				i.remove();
 				c.rides.add(r);
 				c.nextAvailable = c.endTimeRide(currentStep, r);
 				c.pos = r.finish;
 			}
 		}
-			
+
 	}
-	
+
 	public Car getBestCarForRide(Ride ride) {
 		Car tmpCar;
 		int[] choice = new int[cars.size()];
-		for(int i = 0; i< cars.size(); i++) {
+		for (int i = 0; i < cars.size(); i++) {
 			tmpCar = cars.get(i);
 			choice[i] = algoImportant(tmpCar, ride);
 		}
-		
+
 		tmpCar = cars.get(0);
 		int tmp = 0;
-		
-		for(int i = 0; i< choice.length; i++) {
-			if(tmp < choice[i]) {
+
+		for (int i = 0; i < choice.length; i++) {
+			if (tmp < choice[i]) {
 				tmpCar = cars.get(i);
 				tmp = choice[i];
 			}
 		}
-		if(tmp == -1) {
+		if (tmp == -1) {
 			return null;
 		}
 		return tmpCar;
 	}
-	
+
 	public int algoImportant(Car c, Ride r) {
-		if(!c.rideIsPossible(c.nextAvailable, r)) {
+		if (!c.rideIsPossible(c.nextAvailable, r)) {
 			return -1;
 		}
-		
+
 		int total = 0;
 		if(r.earliest == (c.distanceToRide(r)+ c.nextAvailable)) {//chope bonus
 			total += 0.75f*bonus;
@@ -99,34 +99,45 @@ public class Simulation {
   }
 
 	public Car getClosestAvailableCar(Ride ride) {
-		int min_time=10000000;
-		Car closestAvailableCar=null;
-		for(Car car: this.cars) {
-			int time = car.pos.distance(ride.start)+car.nextAvailable-this.currentStep;
-			if(time<min_time) {
-				closestAvailableCar=car;
-				min_time=time;
+		int min_time = 10000000;
+		Car closestAvailableCar = null;
+		for (Car car : this.cars) {
+			int time = car.pos.distance(ride.start) + car.nextAvailable - this.currentStep;
+			if (time < min_time) {
+				closestAvailableCar = car;
+				min_time = time;
 			}
 		}
 		return closestAvailableCar;
 	}
-	
+
 	public void resolveV1() {
 		Collections.shuffle(rides);
 		sortRidesByStartingTime();
 		
 		do {
-			Iterator<Ride> i = rides.iterator();
-			while(i.hasNext()) {
-				Ride r = i.next();
-				
-				Car c = getClosestAvailableCar(r);
-				c.rides.add(r);
-				i.remove();
-				c.nextAvailable = c.endTimeRide(currentStep, r);
-				c.pos = r.finish;
+			for(Car c : cars) {
+				if(c.isAvailable(currentStep)) {
+					
+					for(Ride r : rides) {
+						if(c.rideIsPossible(currentStep, r)) {
+							rides.remove(r);
+							c.rides.add(r);
+							c.nextAvailable = c.endTimeRide(currentStep, r);
+							c.pos = r.finish;
+							
+							break;
+						}
+					}
+				}
 			}
+			
 		currentStep++;
 		}while(currentStep <= nbMaxSteps);
+	}
+
+	public void resolveLouis() {
+		sortRidesByStartingTime();
+
 	}
 }
